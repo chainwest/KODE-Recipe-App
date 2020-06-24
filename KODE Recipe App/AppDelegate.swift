0bea940dc4a26c24a96772b8af19eea970220e34
Jack <evgeniyurbanovkiy@gmail.com>
2020-06-24 14:23:33 +0700

    Initial commit

---
+++ KODE Recipe App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  KODE Recipe App
+//
+//  Created by Evgeniy on 24.06.2020.
+//  Copyright © 2020 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'KODE Recipe App' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for KODE Recipe App
+  pod 'SnapKit'
+  pod 'Alamofire'
+  pod 'Kingfisher'
+
+end
